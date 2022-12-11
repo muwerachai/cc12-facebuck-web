1f3de6bc50587db2c534c5997a336f3bd4dd9641
werachai <werachai@MacBook-Air-khxng-werachai.local>
2022-12-11 10:54:08 +0700

    feature register

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import Router from './route/Router'
+import Router from './route/Router';
+import { ToastContainer } from 'react-toastify';
+import { useLoading } from './contexts/LoadingContext';
+import Spinner from './components/ui/Spinner';
 
 function App() {
-  return <Router />;
+  const { loading } = useLoading();
+
+
+  return (
+  <>
+    {loading && <Spinner />}
+    <Router />
+    <ToastContainer autoClose="2500" theme="colored" />
+  </>
+  );
 }
 
 export default App;
--- src/route/Router.js
+++ src/route/Router.js
@@ -1,13 +1,20 @@
 import { Route,Routes } from 'react-router-dom';
+import { useAuth } from '../contexts/AuthContext';
 import LoginPage from '../pages/LoginPage';
 
+
 function Router(){
+    const { user } = useAuth();
     return (
         <Routes>
-            <Route path="/" element={<LoginPage />}/>
+            {user ? (
+                <>
+                    <Route path="/" element={<PostPage />}/>
+                </>
+            ) : (
+                    <Route path="/" element={<LoginPage />}/>
+            )}
         </Routes>
-
-    );
-    
+    ); 
 }
 export default Router;
